3a6d8fd428f8d36da7834b4557f3210abd31c505
Reny Alvarado <renyalvarado@gmail.com>
2022-03-24 15:39:34 -0300

    Add unit tests for Question model and views

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -9,7 +9,8 @@ class Question(models.Model):
     list_display = ("id", "question_text", "pub_date")
 
     def was_published_recently(self) -> bool:
-        return self.pub_date >= (timezone.now() - datetime.timedelta(days=1))
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     def __repr__(self):
         fields_and_values = [
